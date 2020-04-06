11acdb498d65616192cbf416cd3f1d9cfda80fba
01Petard <1520394133@qq.com>
2020-04-06 10:30:32 +0800

    Add splash.

--- app/src/main/java/com/ixuea/courses/kaimeitu/util/Constants.java
+++ app/src/main/java/com/ixuea/courses/kaimeitu/util/Constants.java
@@ -1,7 +1,7 @@
 package com.ixuea.courses.kaimeitu.util;
 
 public class Constants {
-    public static final String USERNAME = "1520394133@qq.com";
+    public static final String USERNAME = "123@qq.com";
     public static final String PASSWORD = "123456789";
     //模拟接口地址，因为图片请求网址都是这个开头，为了方便就把前面部分作为常量
     public static final String ENDPOINT = "http://rap2api.taobao.org/app/mock/162466/";
